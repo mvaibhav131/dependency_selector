4b79fcda97717b69971467793e03f61bf4132827
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-07-12 12:26:25 +0530

    :crown: main file added to app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DependencySelector from './component/Selector';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Dependency Selector</h2>
+     <DependencySelector/>
+    </div>
+  );
+}
+
+export default App;
