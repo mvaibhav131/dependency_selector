d1ed6ad3c7f5ecf18ac3a765b9b8258dc9b5453b
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-07-12 16:52:58 +0530

    :sparkles: app.jsx done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import DependencySelector from './component/Selector';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Dependency Selector</h2>
+     <DependencySelector/>
+    </div>
+  );
+}
+
+export default App;
