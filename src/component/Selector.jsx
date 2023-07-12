371f2376a5a62565403c5651d2f73cba896e7c4d
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-07-12 13:06:51 +0530

    :sparkles: selector function done()

--- src/component/Selector.jsx
+++ src/component/Selector.jsx
@@ -1,94 +1,94 @@
+
+
 import React, { useState } from 'react';
 import data from '../data/MOCK_DATA.json';
+import Select from 'react-select';
 import './selector.scss';
 
 const DependencySelector = () => {
-    const [selectedCountry, setSelectedCountry] = useState('');
-    const [selectedState, setSelectedState] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
-  
-    const handleCountryChange = (event) => {
-      setSelectedCountry(event.target.value);
-      setSelectedState('');
-      setSelectedCity('');
-    };
-  
-    const handleStateChange = (event) => {
-      setSelectedState(event.target.value);
-      setSelectedCity('');
-    };
-  
-    const handleCityChange = (event) => {
-      setSelectedCity(event.target.value);
-    };
-  
-    return (
-      <div className="container">
-        <div className="selector">
-          <label className="label">Country:</label>
-          <select
-            value={selectedCountry}
-            onChange={handleCountryChange}
-            className="select"
-          >
-            <option value="">Select Country</option>
-            {data.map((item, index) => (
-              <option key={index} value={item.countries.name}>
-                {item.countries.name}
-              </option>
-            ))}
-          </select>
-        </div>
-  
-        <div className="selector">
-          <label className="label">State:</label>
-          <select
-            value={selectedState}
-            onChange={handleStateChange}
-            className="select"
-            disabled={!selectedCountry}
-          >
-            <option value="">Select State</option>
-            {selectedCountry &&
-              data
-                .find((item) => item.countries.name === selectedCountry)
-                .countries.state.name.map((state, index) => (
-                  <option key={index} value={state}>
-                    {state}
-                  </option>
-                ))}
-          </select>
-        </div>
-  
-        <div className="selector">
-          <label className="label">City:</label>
-          <select
-            value={selectedCity}
-            onChange={handleCityChange}
-            className="select"
-            disabled={!selectedState}
-          >
-            <option value="">Select City</option>
-            {selectedState &&
-              data
-                .find((item) => item.countries.name === selectedCountry)
-                .countries.state.cities.map((city, index) => (
-                  <option key={index} value={city}>
-                    {city}
-                  </option>
-                ))}
-          </select>
-        </div>
-  
-        {selectedCity && (
-          <div className="selected-city">
-            <p behavior="" direction="right">Selected Country: {selectedCountry}</p>
-            <p behavior="" direction="right">Selected State: {selectedState}</p>
-            <p behavior="" direction="right">Selected City: {selectedCity}</p>
-          </div>
-        )}
-      </div>
-    );
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedState, setSelectedState] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const handleCountryChange = (selectedOption) => {
+    setSelectedCountry(selectedOption);
+    setSelectedState(null);
+    setSelectedCity(null);
   };
-  
-  export default DependencySelector;
+
+  const handleStateChange = (selectedOption) => {
+    setSelectedState(selectedOption);
+    setSelectedCity(null);
+  };
+
+  const handleCityChange = (selectedOption) => {
+    setSelectedCity(selectedOption);
+  };
+
+  const countryOptions = data.map((item) => ({
+    value: item.countries.name,
+    label: item.countries.name,
+  }));
+
+  const stateOptions =
+    selectedCountry &&
+    data
+      .find((item) => item.countries.name === selectedCountry.value)
+      ?.countries.state.name.map((state) => ({
+        value: state,
+        label: state,
+      }));
+
+  const cityOptions =
+    selectedState &&
+    data
+      .find((item) => item.countries.name === selectedCountry.value)
+      ?.countries.state.cities.map((city) => ({
+        value: city,
+        label: city,
+      }));
+
+  return (
+    <div className="container">
+      <div className="selector">
+        <label className="label">Country:</label>
+        <Select
+          value={selectedCountry}
+          onChange={handleCountryChange}
+          options={countryOptions}
+          isClearable
+        />
+      </div>
+
+      <div className="selector">
+        <label className="label">State:</label>
+        <Select
+          value={selectedState}
+          onChange={handleStateChange}
+          options={stateOptions}
+          isClearable
+          isDisabled={!selectedCountry}
+        />
+      </div>
+
+      <div className="selector">
+        <label className="label">City:</label>
+        <Select
+          value={selectedCity}
+          onChange={handleCityChange}
+          options={cityOptions}
+          isClearable
+          isDisabled={!selectedState}
+        />
+      </div>
+
+      {selectedCity && (
+        <div className="selected-city">
+          Selected City: {selectedCity.label} ({selectedState.label}, {selectedCountry.label})
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DependencySelector;
