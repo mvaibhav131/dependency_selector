0e83444d56711f0f2c5c3bfc1ac3f8343dfdfa00
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-07-19 10:01:01 +0530

    :+1:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import DependencySelector from './component/Selector';
+import KrishnaBackground from './component/Template';
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Dependency Selector</h2>
-     <DependencySelector/>
+      {/* <h2>Dependency Selector</h2>
+     <DependencySelector/> */}
+     <KrishnaBackground/>
     </div>
   );
 }
