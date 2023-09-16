1d47ce184d9455413450087fba92db177066839c
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-09-16 22:47:20 +0530

    template added to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import Banner from './component/Banner';
-import DependencySelector from './component/Selector';
-
+// import DependencySelector from './component/Selector';
+import Template from './component/template';
 
 function App() {
   return (
     <div className="App">
       {/* <h2>Dependency Selector</h2>
      <DependencySelector/> */}
-     <Banner/>
+     {/* <Banner/> */}
+     <Template/>
     </div>
   );
 }
