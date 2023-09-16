4aaa87af1c828874417916e11b8d8d013300aa42
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-09-16 22:47:39 +0530

    teamplate js

---
+++ src/component/template.js
@@ -0,0 +1,22 @@
+// Template.js
+
+import React from 'react';
+import './teamplate.css';
+
+const Template = () => {
+  return (
+    <div className="template">
+      <div className="overlay"></div>
+      <div className="background-image"></div>
+      <div className="content">
+        <h1 className="heading1">INTRODUCTION TO</h1>
+        <h2 className="heading2">ARRAY & STRING</h2>
+        <h3 className="heading3">IN JAVASCRIPT</h3>
+        <h3 className="heading4">@mvaibhav131</h3>
+        <h3 className="img"><img src="https://miro.medium.com/v2/resize:fit:1200/1*yUNfohs9jA6GCDmyCYJTvA@2x.png"/></h3>
+      </div>
+    </div>
+  );
+};
+
+export default Template;
