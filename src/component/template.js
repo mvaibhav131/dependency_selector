b9c25a08ad0ec0207805a593e94cb99b98ddc045
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-09-17 00:36:34 +0530

    template new js

--- src/component/template.js
+++ src/component/template.js
@@ -9,8 +9,8 @@ const Template = () => {
       <div className="overlay"></div>
       <div className="background-image"></div>
       <div className="content">
-        <h1 className="heading1">INTRODUCTION TO</h1>
-        <h2 className="heading2">ARRAY & STRING</h2>
+        <h1 className="heading1">DATA STRUCTURES & ALGORITHMS</h1>
+        <h2 className="heading2">ROTATE MATRIX &#91;&#93;</h2>
         <h3 className="heading3">IN JAVASCRIPT</h3>
         <h3 className="heading4">@mvaibhav131</h3>
         <h3 className="img"><img src="https://miro.medium.com/v2/resize:fit:1200/1*yUNfohs9jA6GCDmyCYJTvA@2x.png"/></h3>
