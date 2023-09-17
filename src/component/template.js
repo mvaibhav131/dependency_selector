37cf80e03c92e0572751694bf4060a8ee4d63c6e
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-09-17 15:25:35 +0530

    teamplate js

--- src/component/template.js
+++ src/component/template.js
@@ -9,11 +9,11 @@ const Template = () => {
       <div className="overlay"></div>
       <div className="background-image"></div>
       <div className="content">
-        <h1 className="heading1">LEARN</h1>
-        <h2 className="heading2">DATA STRUCTURES & ALGORITHMS</h2>
-        <h3 className="heading3">IN JAVASCRIPT</h3>
+        <h1 className="heading1">HOW TO RUN ?</h1>
+        <h2 className="heading2">ELECTRON JS</h2>
+        <h3 className="heading3">APPLICATIONS</h3>
         <h3 className="heading4">@mvaibhav131</h3>
-        <h3 className="img"><img src="https://miro.medium.com/v2/resize:fit:1200/1*yUNfohs9jA6GCDmyCYJTvA@2x.png"/></h3>
+        <h3 className="img"><img src="https://miro.medium.com/v2/resize:fit:1200/1*O6KluMvEBZ1cBL3EPo4tig.png"/></h3>
       </div>
     </div>
   );
